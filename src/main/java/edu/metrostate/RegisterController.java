a0a6cd21a2bbea06a6851cdf7419f951b50f34e7
Julia Zinda <di1585tx@go.minnstate.edu>
2024-04-01 18:54:28 -0500

    submit button needs work

--- src/main/java/edu/metrostate/RegisterController.java
+++ src/main/java/edu/metrostate/RegisterController.java
@@ -1,13 +1,63 @@
 package edu.metrostate;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class RegisterController {
+public class RegisterController implements Initializable {
+    @FXML
+    private Button submitButton;
+    @FXML
+    private Button returnToLoginButton;
 
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField lastNameTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private TextField phoneTextField;
+    @FXML
+    private TextField setPasswordField;
+    @FXML
+    private TextField confirmPasswordField;
+
+    @FXML
+    private DatePicker dateOfBirthPicker;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(!firstNameTextField.getText().trim().isEmpty() && !lastNameTextField.getText().trim().isEmpty() && !phoneTextField.getText().trim().isEmpty() && !emailTextField.getText().trim().isEmpty() && !setPasswordField.getText().trim().isEmpty()){
+                    //Database.registerUser(actionEvent, firstNameTextField.getText(), lastNameTextField.getText(), phoneTextField.getText(), emailTextField.getText(), setPasswordField.getText());
+                } else {
+                    System.out.println("Please fill out all fields.");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Please fill out all the fields to register.");
+                    alert.show();
+                }
+
+            }
+        });
+
+        returnToLoginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DBUtils.changeScene(actionEvent, "login.fxml"); //insert tabular main page for fxml file
+            }
+        });
+    }
+
+   // public
 }
