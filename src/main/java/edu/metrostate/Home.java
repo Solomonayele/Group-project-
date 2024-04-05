ca70b4c0db06386d1aa6283ca4be0425e3a7d4b8
Solomonayele <97987055+Solomonayele@users.noreply.github.com>
2024-04-04 19:54:18 -0500

    Home page

---
+++ src/main/java/edu/metrostate/Home.java
@@ -0,0 +1,23 @@
+package edu.metrostate;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class Home {
+
+    @FXML
+    private AnchorPane A;
+
+    @FXML
+    private Label logOutID;
+
+    @FXML
+    private Button bookApptID;
+
+    @FXML
+    private Button cancelApptID;
+
+}
+
